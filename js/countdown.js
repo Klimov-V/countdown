944a4a97d4d1c37edcaf36d1a89dcb6e15b29474
Klimov-V <klimov_vitaliy@icloud.com>
2020-05-26 20:42:08 +0300

    initial commit

---
+++ js/countdown.js
@@ -0,0 +1,81 @@
+(() => {
+    'use strict';
+
+    class Countdown {
+
+        _secinds_per_day = 60 * 60 * 24;
+        _secinds_per_hour = 60 * 60;
+        _secinds_per_minute = 60;
+        
+        
+
+        constructor(options) {
+            this.due_date = new Date(options.due_date);
+            this.target = options.target;
+
+            if (this.target) {
+                this.calc();
+                this.display();
+                
+                this.start()
+            }
+        }
+
+        display() {
+            let hours = ('0' + this.hours).slice(-2),
+                minutes = ('0' + this.minutes).slice(-2),
+                seconds = ('0' + this.seconds).slice(-2);
+
+            this.target.innerHTML = `${this.days} : ${hours} : ${minutes} : ${seconds}`
+
+            // document.write(`${this.days} : ${hours} : ${minutes} : ${seconds}`);
+        }
+
+        calc() {
+            this.now = new Date();
+
+            this.delta = Math.round((this.due_date - this.now) / 1000);
+
+            this.delta = this.delta > 0 ? this.delta : 0;
+
+            let tmp = this.delta;
+
+            this.days = Math.floor(tmp / this._secinds_per_day);
+            tmp = tmp - this.days * this._secinds_per_day;
+
+            this.hours = Math.floor(tmp / this._secinds_per_hour);
+            tmp = tmp - this.hours * this._secinds_per_hour;
+
+            this.minutes = Math.floor(tmp / this._secinds_per_minute);
+            tmp = tmp - this.minutes * this._secinds_per_minute;
+            
+            this.seconds = tmp;
+
+            if (this.delta == 0) {
+                this.stop();
+            }
+        }
+
+        start() {
+            this.id = setInterval(() => {
+                this.calc();
+                this.display();
+            }, 1000);
+        }
+
+        stop() {
+            clearInterval(this.id);
+        }
+    }
+
+    let newYearCountdown = new Countdown({
+        target: document.querySelector('#newYear'),
+        due_date : '2021-01-01 00:00:00'
+    });
+
+    let farewellPartyCountdown = new Countdown({
+        target: document.querySelector('#farewellParty'),
+        due_date : '2020-07-03 18:30:00'
+    });
+    
+})();
